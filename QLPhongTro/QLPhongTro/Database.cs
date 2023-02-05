b2fa5a3a046f968f4f9a0beaca4bfb0c39cbe93e
Dang Thuy Do <dodangthuy83@yahoo.com>
2023-02-05 16:06:11 +0700

    create connectstring

---
+++ QLPhongTro/QLPhongTro/Database.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLPhongTro
+{
+    internal class Database
+    {
+        private string connectionString = @"Data Source = Localhost\SQLSERVER; Initial Catalog = QLPHONGTRO; user ID = sa; password = 123";
+        private SqlConnection con;
+        private DataTable dt;
+        private SqlCommand cmd;
+    }
+}
